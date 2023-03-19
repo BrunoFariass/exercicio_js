f01f53d0197c9e0ab37c7488684820dbef1eff96
Bruno Farias Nasente <fariasnasentebruno.pt@gmail.com>
2023-03-19 00:33:26 +0000

    alterando cod main.js

--- main.js
+++ main.js
@@ -8,8 +8,10 @@ const primeiroCampo = document.getElementById('campo-a').value;
 const segundoCampo = document.getElementById('campo-b').value;
 
 if (primeiroCampo == segundoCampo) {
-    alert('Os números são iguais');
-} else if (segundoCampo > primeiroCampo) {
+    alert('Os campos possuem o mesmo valor. Por Favor digite um valor diferente:');
+} else if (primeiroCampo > segundoCampo) {
+    alert('O campo A é maior que B');
+} else { (segundoCampo > primeiroCampo); 
     alert('O campo B é maior que A')
 }
 
